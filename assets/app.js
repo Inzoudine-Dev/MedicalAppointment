3270a2a3c2434ca32d8e617d617f7a6127bf3c96
Maham <mahamoud-inzoudine@hotmail.fr>
2024-09-16 23:03:37 +0200

    Initial commit

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,17 @@
-import './bootstrap.js';
+//import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+
+/*imports css*/
+//import './styles/app.css';
+import './styles/users/usersHeader.css';
+
+/*imports javascripts*/
+//import './javascripts/scriptTest.js';
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
